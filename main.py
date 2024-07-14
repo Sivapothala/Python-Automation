790c0407119e9004098976439db313458ad0fd8c
siva  Pothala <35196558-sivapothala@users.noreply.replit.com>
2024-07-14 05:31:20 +0000

    basics of scraping

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("Hello World")
+from selenium import webdriver
+
+# Web driver is tool allow us to control the browser.
+
+def get_driver():
+  options = webdriver.ChromeOptions()
+  options.add_argument("disable-infobars") # disable the info bars
+  options.add_argument("start-maximized") # start the browser in maximized mode
+  options.add_argument("disable-dev-shm-usage") # disable the use of the dev shm where we try to use from linux. some issues may occur. to avoid that.
+  options.add_argument("no-sandbox")
+  options.add_experimental_option("excludeSwitches", ["enable-automation"]) # disable the automation
+  options.add_argument("disable-blink-features=AutomationControlled") 
+  
+  driver = webdriver.Chrome(options)
+  driver.get("http://automated.pythonanywhere.com")
+  return driver
+
+def main():
+  driver = get_driver()
+  element = driver.find_element(by="xpath", value="/html/body/div[1]/div/h1[1]")
+  return element.text
+
+print(main())
+
